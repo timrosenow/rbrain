a3415a3e1d405be034e34a9a4ab47654c788f442
Tim Rosenow <Tim.Rosenow@uwa.edu.au>
2023-01-23 11:50:53 +0800

    Initial upload

--- rbrain/__init__.py
+++ rbrain/__init__.py
@@ -1,4 +1,10 @@
 """
+rbrain.
+
 MRI analysis software for rodent brains.
 """
+__version__ = '0.1.0'
+__author__ = 'Tim Rosenow'
+__credits__ = 'National Imaging Facility'
+
 import rbrain.segmenter
